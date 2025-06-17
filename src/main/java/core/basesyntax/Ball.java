aa567da9ad3cd0d59e89e451894fbf6fe4077b18
Yuliia Vasylieva <99039851+Qellexi@users.noreply.github.com>
2025-06-17 17:04:41 +0300

    Create Ball.java

---
+++ src/main/java/core/basesyntax/Ball.java
@@ -0,0 +1,26 @@
+public class Ball {
+  private int number;
+  private String color;
+
+  public Ball(String color, int number) {
+    this.color = color;
+    this.number = number;
+  }
+
+  public String getColor() {
+    return color;
+  }
+
+  public int getNumber() {
+    return number;
+  }
+
+  @Override
+  public String toString() {
+    return "Ball (color: " + color + ", number: " + number + ")";
+  }
+}
+
+public enum Color {
+    RED, GREEN, BLUE;
+}
