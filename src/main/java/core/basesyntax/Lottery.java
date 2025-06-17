d472d9610b76e69a4c485907c11a3307cf25c8d2
Yuliia Vasylieva <99039851+Qellexi@users.noreply.github.com>
2025-06-17 18:27:04 +0300

    Update Lottery.java

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -6,7 +6,6 @@ public class Lottery {
     public Ball getRandomBall() {
         Random random = new Random();
         ColorSupplier color = new ColorSupplier();
-        Ball ball = new Ball(color.getRandomColor(), random.nextInt(100));
-        return ball;
+        return new Ball(color.getRandomColor(), random.nextInt(100));
     }
 }
